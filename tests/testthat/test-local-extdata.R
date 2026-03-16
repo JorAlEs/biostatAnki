a211fd189f34b8445a330b4ab2784873be6f9885
Jorge A <jorge.alcantara.espinosa@hotmail.com>
2026-03-16 10:03:28 +0100

    test: cover local knowledge repository loading

--- tests/testthat/test-local-extdata.R
+++ tests/testthat/test-local-extdata.R
@@ -16,12 +16,22 @@ test_that(".load_package_csv prefers source checkout data when available", {
     ),
     file.path(temp_root, "inst", "extdata", "questions.csv")
   )
+  writeLines(
+    c(
+      "section,topic,summary,test_area_reference",
+      "\"R\",\"Local topic\",\"Loaded from source checkout\",\"Local tests\""
+    ),
+    file.path(temp_root, "inst", "extdata", "knowledge_repository.csv")
+  )
 
   old_wd <- setwd(nested_dir)
   on.exit(setwd(old_wd), add = TRUE)
 
   qdf <- biostatAnki:::.load_package_csv("questions.csv")
+  knowledge <- biostatAnki:::.load_package_csv("knowledge_repository.csv")
 
   expect_equal(as.integer(qdf$id[[1]]), 999L)
   expect_equal(qdf$question[[1]], "Local source question")
+  expect_equal(knowledge$topic[[1]], "Local topic")
+  expect_equal(knowledge$summary[[1]], "Loaded from source checkout")
 })
